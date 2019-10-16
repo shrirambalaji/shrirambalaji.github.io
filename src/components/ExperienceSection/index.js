fc1a86141c7585328a687827b8b113f787f921d5
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-16 15:48:00 +0530

    wip: refactor experienceSection

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -1,36 +1,49 @@
-import React from 'react'
-import Section from '../Section'
-import ExperienceUnit from '../ExperienceUnit'
+import { graphql, StaticQuery } from "gatsby";
+import React from "react";
+import CiscoLogo from "../../assets/images/experience/cisco-logo.jpg";
+import NetAppLogo from "../../assets/images/experience/netapp-logo.svg";
+import ExperienceUnit from "../ExperienceUnit";
+import Section from "../Section";
 
-import NetAppLogo from '../../assets/images/experience/netapp-logo.svg'
-import CiscoLogo from '../../assets/images/experience/cisco-logo.jpg'
+const ExperienceSection = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query ExperienceQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => {
+        return (
+          <Section title="💼&nbsp;&nbsp;Experience">
+            <div className="row">
+              <ExperienceUnit
+                logo={NetAppLogo}
+                colour="#fff"
+                title="NetApp"
+                link="https://www.netapp.com/"
+                timeperiod="2018 - Present"
+                subtitle="Building state-of-the-art tools for the enhanced developer productivity."
+              />
+              <ExperienceUnit
+                logo={CiscoLogo}
+                colour="#FFFFFF"
+                title="Cisco"
+                link="https://www.cisco.com/"
+                timeperiod="2017 - 2018"
+                subtitle="Built scalable node.js micro-service architectures as part of the Engineering Team in Team-
+            One, a cloud-based messaging and collaboration tool."
+              />
+            </div>
+          </Section>
+        );
+      }}
+    />
+  );
+};
 
-class ExperienceSection extends React.Component {
-  render() {
-    return (
-      <Section title="💼&nbsp;&nbsp;Experience">
-        <div className="row">
-          <ExperienceUnit
-            logo={NetAppLogo}
-            colour='#fff'
-            title='NetApp'
-            link='https://www.netapp.com/'
-            timeperiod='2018 - Present'
-            subtitle='Building state-of-the-art tools for the enhanced developer productivity.'
-          />
-          <ExperienceUnit
-            logo={CiscoLogo}
-            colour='#FFFFFF'
-            title='Cisco'
-            link='https://www.cisco.com/'
-            timeperiod='2017 - 2018'
-            subtitle='Built scalable node.js micro-service architectures as part of the Engineering Team in Team-
-            One, a cloud-based messaging and collaboration tool.'
-          />
-        </div>
-      </Section>
-    )
-  }
-}
-
-export default ExperienceSection
+export default ExperienceSection;
