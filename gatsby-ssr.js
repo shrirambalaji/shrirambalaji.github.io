8ed0593c45bf6306316ce9b2465aa723f480519d
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-16 15:44:26 +0530

    use only cjs format in gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,18 +1,18 @@
-import Helmet from "react-helmet"
+const { Helmet } = require("react-helmet");
 
 exports.onRenderBody = (
   { setHeadComponents, setHtmlAttributes, setBodyAttributes },
   pluginOptions
 ) => {
-  const helmet = Helmet.renderStatic()
-  setHtmlAttributes(helmet.htmlAttributes.toComponent())
-  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  const helmet = Helmet.renderStatic();
+  setHtmlAttributes(helmet.htmlAttributes.toComponent());
+  setBodyAttributes(helmet.bodyAttributes.toComponent());
   setHeadComponents([
     helmet.title.toComponent(),
     helmet.link.toComponent(),
     helmet.meta.toComponent(),
     helmet.noscript.toComponent(),
     helmet.script.toComponent(),
-    helmet.style.toComponent(),
-  ])
-}
+    helmet.style.toComponent()
+  ]);
+};
