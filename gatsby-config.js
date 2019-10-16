8985c141321fa4ba76922ebe7222871ada14981b
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-16 15:48:45 +0530

    chore: update gatsby configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,35 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-sass',
+    "gatsby-plugin-postcss",
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: "gatsby-plugin-sass",
       options: {
-        fonts: [
-          `space mono\:300, 500, 600, 900`
-        ]
+        cssLoaderOptions: {
+          camelCase: false
+        }
+        // fonts: [`space mono\:300, 500, 600, 900`]
       }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "<your-tracking-id-here>",
-        head: true
-      }
-    },
+    }
   ],
   siteMetadata: {
-    title: 'Shriram Balaji',
-    description: 'Software Engineer',
-    keywords: 'full stack, product enginner, software engineer, developer, cisco, netapp, portfolio, personal website',
-    url: 'https://www.shriram-balaji.github.io'
+    title: "Shriram Balaji",
+    description: "Software Engineer",
+    keywords:
+      "full stack, product enginner, software engineer, developer, cisco, netapp, portfolio, personal website",
+    url: "https://www.shriram-balaji.github.io",
+    experience: [
+      {
+        company: "NetApp",
+        duration: "Oct 2018 - Present",
+        description:
+          "Built web applications and microservices tailored towards designing and sizing NetApp Storage Solutions."
+      },
+      {
+        company: "Cisco",
+        duration: "June 2017 - Oct 2018",
+        description:
+          "Built scalable microservice architectures and reliable Message Queueing Systems as part of the Engineering Team in Team-One a cloud based messaging tool, now part of WebEx."
+      }
+    ]
   }
 };
