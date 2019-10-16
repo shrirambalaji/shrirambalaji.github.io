c4b9b34fedad3c67bd77e217742367515b7b396d
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-16 15:43:51 +0530

    refactor: move layout file into components

---
+++ src/components/layout.js
@@ -0,0 +1,48 @@
+import { StaticQuery, graphql } from "gatsby";
+import React from "react";
+import Helmet from "react-helmet";
+import favicon16 from "../assets/favicons/favicon-16x16.png";
+import favicon32 from "../assets/favicons/favicon-32x32.png";
+import profileImage from "../assets/favicons/favicon.png";
+import "./style.scss";
+
+export default ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query HeadingQuery {
+        site {
+          siteMetadata {
+            title
+            description
+            keywords
+            url
+          }
+        }
+      }
+    `}
+    render={({ site }) => (
+      <div className="template-wrapper">
+        <Helmet
+          title={site.siteMetadata.title}
+          meta={[
+            { name: "description", content: site.siteMetadata.description },
+            { name: "keywords", content: site.siteMetadata.keywords },
+            { property: "og:url", content: site.siteMetadata.url },
+            { property: "og:image", content: profileImage },
+            { property: "og:title", content: site.siteMetadata.title },
+            {
+              property: "og:description",
+              content: site.siteMetadata.description
+            }
+          ]}
+          link={[
+            { rel: "icon", type: "image/png", sizes: "16x16", href: favicon16 },
+            { rel: "icon", type: "image/png", sizes: "32x32", href: favicon32 }
+          ]}
+        />
+        <div className="template-wrapper-children">{children}</div>
+      </div>
+    )}
+  />
+);
+
