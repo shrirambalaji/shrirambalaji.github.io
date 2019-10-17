e11c03a4ad4401fdfc5f3cb0042d51d60d21d88e
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-18 00:16:25 +0530

    refactor: About Component to FC

--- src/components/Sidebar/About/index.js
+++ src/components/Sidebar/About/index.js
@@ -1,31 +1,54 @@
-import React, { Component } from "react";
-
+import { graphql, StaticQuery } from "gatsby";
+import React, { Fragment } from "react";
 import "./style.scss";
 
-class About extends Component {
-  render() {
-    return (
-      <div className="about">
-        <div className="image"></div>
-        <div className="bio">
-          Software Engineer ∙ Open Sourcerer ∙ Tech Enthusiast
-          <div className="emoji">
-            <span role="img" aria-label="man-typing-emoji">
-              👨🏽‍💻
-            </span>
-            &nbsp;
-            <span role="img" aria-label="man-typing-emoji">
-              😍
-            </span>
-            &nbsp;
-            <span role="img" aria-label="man-typing-emoji">
-              🤖
-            </span>
-          </div>
+const About = () => {
+  return (
+    <div className="about">
+      <div className="image"></div>
+      <div className="bio">
+        <div>
+          <StaticQuery
+            query={graphql`
+              query LinkEmojiQuery {
+                allMarkdownRemark {
+                  nodes {
+                    frontmatter {
+                      bio
+                      misc {
+                        emojis
+                      }
+                    }
+                  }
+                }
+              }
+            `}
+            render={data => {
+              const {
+                misc,
+                bio
+              } = data.allMarkdownRemark.nodes[0].frontmatter;
+              const { emojis } = misc[0];
+              return (
+                <Fragment>
+                  <p>{bio}</p>
+                  {emojis.map(emoji => (
+                    <span
+                      className="emoji"
+                      role="img"
+                      aria-label="sidelink-emoji"
+                    >
+                      &nbsp;{emoji}&nbsp;
+                    </span>
+                  ))}
+                </Fragment>
+              );
+            }}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default About;
