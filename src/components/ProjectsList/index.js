c1f37fe09ab0138fed52848ae0e15921ff9b0a08
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-17 23:58:46 +0530

    refactor: ProjectsSection to ProjectsList; convert to FC

---
+++ src/components/ProjectsList/index.js
@@ -0,0 +1,57 @@
+import { graphql, StaticQuery } from "gatsby";
+import React from "react";
+import Section from "../Section";
+import SectionItem from "../SectionItem";
+
+const ProjectsList = () => {
+  return (
+    <Section title="Projects">
+      <div className="row">
+        <StaticQuery
+          query={graphql`
+            query ProjectsQuery {
+              allMarkdownRemark {
+                nodes {
+                  frontmatter {
+                    projects {
+                      description
+                      duration
+                      link
+                      name
+                      tags
+                      wip
+                      title
+                    }
+                  }
+                }
+              }
+            }
+          `}
+          render={({ allMarkdownRemark }) => {
+            if (!allMarkdownRemark) return null;
+            const frontmatter =
+              allMarkdownRemark.nodes[0] &&
+              allMarkdownRemark.nodes[0].frontmatter;
+
+            const { projects } = frontmatter;
+            return projects.map(
+              ({ title, description, duration, tags, link }) => {
+                return (
+                  <SectionItem
+                    type="project"
+                    title={title}
+                    link={link}
+                    timeperiod={duration}
+                    subtitle={description}
+                  />
+                );
+              }
+            );
+          }}
+        ></StaticQuery>
+      </div>
+    </Section>
+  );
+};
+
+export default ProjectsList;
