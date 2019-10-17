e21b43a73285456b64ba0752bc500f4bc81f233c
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-18 01:21:22 +0530

    fix: move data-browser mixin to root div in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import { StaticQuery, graphql } from "gatsby";
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Helmet from "react-helmet";
 import favicon16 from "../assets/favicons/favicon-16x16.png";
 import favicon32 from "../assets/favicons/favicon-32x32.png";
@@ -20,29 +20,46 @@ export default ({ children }) => (
         }
       }
     `}
-    render={({ site }) => (
-      <div className="template-wrapper">
-        <Helmet
-          title={site.siteMetadata.title}
-          meta={[
-            { name: "description", content: site.siteMetadata.description },
-            { name: "keywords", content: site.siteMetadata.keywords },
-            { property: "og:url", content: site.siteMetadata.url },
-            { property: "og:image", content: profileImage },
-            { property: "og:title", content: site.siteMetadata.title },
-            {
-              property: "og:description",
-              content: site.siteMetadata.description
-            }
-          ]}
-          link={[
-            { rel: "icon", type: "image/png", sizes: "16x16", href: favicon16 },
-            { rel: "icon", type: "image/png", sizes: "32x32", href: favicon32 }
-          ]}
-        />
-        <div className="template-wrapper-children">{children}</div>
-      </div>
-    )}
+    render={({ site }) => {
+      const [userAgent, setUserAgent] = useState(null);
+      useEffect(() => {
+        const ua =
+          typeof window !== `undefined` ? window.navigator.userAgent : null;
+        setUserAgent(ua);
+      }, []);
+      return (
+        <div className="root" data-browser={userAgent}>
+          <Helmet
+            title={site.siteMetadata.title}
+            meta={[
+              { name: "description", content: site.siteMetadata.description },
+              { name: "keywords", content: site.siteMetadata.keywords },
+              { property: "og:url", content: site.siteMetadata.url },
+              { property: "og:image", content: profileImage },
+              { property: "og:title", content: site.siteMetadata.title },
+              {
+                property: "og:description",
+                content: site.siteMetadata.description
+              }
+            ]}
+            link={[
+              {
+                rel: "icon",
+                type: "image/png",
+                sizes: "16x16",
+                href: favicon16
+              },
+              {
+                rel: "icon",
+                type: "image/png",
+                sizes: "32x32",
+                href: favicon32
+              }
+            ]}
+          />
+          <Fragment>{children}</Fragment>
+        </div>
+      );
+    }}
   />
 );
-
