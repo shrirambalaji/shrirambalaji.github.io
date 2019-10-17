de08725b29d3406956eaddf3fafd49b6c54fea20
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-17 23:54:23 +0530

    rename: ExperienceSection to ExperienceList

---
+++ src/components/ExperienceList/index.js
@@ -0,0 +1,53 @@
+import { graphql, StaticQuery } from "gatsby";
+import React from "react";
+import SectionItem from "../SectionItem";
+import Section from "../Section";
+
+const ExperienceList = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query ExperienceQuery {
+          allMarkdownRemark {
+            nodes {
+              frontmatter {
+                experience {
+                  company
+                  description
+                  duration
+                  link
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={({ allMarkdownRemark }) => {
+        if (!allMarkdownRemark) return null;
+        const frontmatter =
+          allMarkdownRemark.nodes[0] && allMarkdownRemark.nodes[0].frontmatter;
+
+        const { experience: experienceContents } = frontmatter;
+        return (
+          <Section title="Experience" className="experience">
+            <div className="row">
+              {experienceContents.map(
+                ({ company, description, duration, link }) => (
+                  <SectionItem
+                    type="experience"
+                    link={link}
+                    title={company}
+                    timeperiod={duration}
+                    subtitle={description}
+                  />
+                )
+              )}
+            </div>
+          </Section>
+        );
+      }}
+    />
+  );
+};
+
+export default ExperienceList;
