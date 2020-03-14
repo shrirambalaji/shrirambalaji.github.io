d99835a21441673cc5497aa8d70213d0a39dc101
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-03-15 03:41:50 +0530

    feat(about):  update bio
    * detect emojiSupport before rendering emojis
    * update avatar styles to give more space above head.

--- src/components/Sidebar/About/index.js
+++ src/components/Sidebar/About/index.js
@@ -1,3 +1,4 @@
+import emojiSupport from "detect-emoji-support";
 import { graphql, StaticQuery } from "gatsby";
 import React, { Fragment } from "react";
 import "./style.scss";
@@ -11,34 +12,33 @@ const About = () => {
           <StaticQuery
             query={graphql`
               query LinkEmojiQuery {
-                allMarkdownRemark {
-                  nodes {
-                    frontmatter {
+                markdownRemark {
+                  frontmatter {
+                    about {
                       bio
-                      misc {
-                        emojis
-                      }
+                      emojis
                     }
                   }
                 }
               }
             `}
             render={data => {
-              const { misc, bio } = data.allMarkdownRemark.nodes[0].frontmatter;
-              const { emojis } = misc[0];
+              const { about } = data.markdownRemark.frontmatter;
+              const { bio, emojis } = about;
               return (
                 <Fragment>
-                  <p>{bio}</p>
-                  {emojis.map((emoji, emojiIdx) => (
-                    <span
-                      key={emojiIdx}
-                      className="emoji"
-                      role="img"
-                      aria-label="sidelink-emoji"
-                    >
-                      &nbsp;{emoji}&nbsp;
-                    </span>
-                  ))}
+                  <p>{bio.join(" ∙ ")}</p>
+                  {emojiSupport() &&
+                    emojis.map((emoji, emojiIdx) => (
+                      <span
+                        key={emojiIdx}
+                        className="emoji"
+                        role="img"
+                        aria-label="sidelink-emoji"
+                      >
+                        &nbsp;{emoji}&nbsp;
+                      </span>
+                    ))}
                 </Fragment>
               );
             }}
