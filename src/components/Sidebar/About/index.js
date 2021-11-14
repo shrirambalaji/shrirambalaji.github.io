568d853e525654eb88bffdbe5f359ffd197fb243
Shriram Balaji <shrirambalaji1996@gmail.com>
2021-11-14 12:04:37 +0530

    Fix sidebar padding; Use Image Component for avatar

--- src/components/Sidebar/About/index.js
+++ src/components/Sidebar/About/index.js
@@ -1,12 +1,13 @@
 import emojiSupport from "detect-emoji-support";
 import React, { Fragment } from "react";
 import styles from "./About.module.scss";
+import Image from "next/image";
 
 const About = ({ content: about }) => {
   const { emojis, bio } = about;
   return (
     <div className={styles.about}>
-      <div className={styles.image}></div>
+      <Image className={styles.image} src="/images/avatar.jpg" width={125} height={125} />
       <div className={styles.bio}>
         <div>
           <Fragment>
