d785c0836a589157fc0b808811624c8001b8109e
Shriram Balaji <shrirambalaji1996@gmail.com>
2021-11-14 12:52:06 +0530

    Updates

--- _next/static/xqtWVxkpi73nOt4It5nCa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/950-a3343796e2283f83.js","static/css/ccd4e22e3454b256.css","static/chunks/pages/index-cbb32093b5998fe3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
